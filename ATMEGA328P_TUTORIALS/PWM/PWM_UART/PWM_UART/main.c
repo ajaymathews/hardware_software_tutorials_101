d6d40ecd224a430d0c6045e554d6f4dd34a9b4df
Ajay Mathews <37361585+ajaymathews@users.noreply.github.com>
2023-03-25 16:51:18 +0100

    Add files via upload

---
+++ ATMEGA328P_TUTORIALS/PWM/PWM_UART/PWM_UART/main.c
@@ -0,0 +1,76 @@
+/*
+ * PWM_timer0.c
+ *
+ * Created: 02-06-2019 22:37:28
+ * Author : AJAY
+ */ 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#define F_CPU 8000000L
+#define BAUD 9600
+#define NORMAL_BAUD ((F_CPU/16/BAUD)-1) //corresponding value is 5
+double DutyCycle;
+double reg_value;
+
+
+char c; // needed bcz without it only 1 commnd be executed,ie1st cmmnd is identified only
+void recieve_setup()
+{
+	UBRR0=NORMAL_BAUD;//SETTING BAUD RATE
+	UCSR0B=(1<<RXEN0);//Enables reception and transmission to replay //corrspndg reg value :0x10
+	UCSR0C=((1<<UCSZ00)|(1<<UCSZ01));// enables 8 bit transmission/reception   //corrspndg reg value :0x06
+}
+void pwm_setup()
+{	
+	TCCR0A=((1 << COM0A1) | (1<<WGM01) | (1<<WGM00));// CLEAR OCROA ON COMPARE MATCH AND ENABLE FAST PWM
+	TCCR0B=(1<<CS02)|(1<<CS00);//prescalar on /1024
+}
+void pwm_interrupt()
+{
+		TIMSK0=(1<<TOIE0);
+		sei();
+}
+void get_from_uart()
+{
+	c=UDR0;
+	DutyCycle=(double)c;
+	reg_value=((DutyCycle/10)*255.0);
+	return(reg_value);
+}
+int main(void)
+{
+	DDRB=0xFF;
+	PORTB=0x00;
+	
+	DDRD=1<<PORTD6;
+    recieve_setup();
+	pwm_setup();
+	//pwm_interrupt();
+	OCR0A=get_from_uart();
+    while (1) 
+    {	
+		get_from_uart();
+		OCR0A=get_from_uart();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+ISR(TIMER0_OVF_vect)
+{
+		DutyCycle=10;
+		OCR0A=((DutyCycle/100)*255);
+		PORTB=~PORTB;
+	
+}
+
+
---
+++ ATMEGA328P_TUTORIALS/SWITCH_LED/SWITCH_LED/main.c
@@ -0,0 +1,38 @@
+/*
+ * SWITCH_LED.c
+ *
+ * Created: 31-05-2019 11:03:33
+ * Author : AJAY
+ */ 
+
+//DEBOUNCE
+
+#include <avr/io.h>
+#define 16000000
+
+int main(void)
+{
+	int C=0;
+	DDRB=0x01;
+	PORTB=0x02;  // THE REGISTER THAT STORES THIS PORT VALUES IS 'PINB', APPLING A 5V TO THE 2ND PIN OF B PORT 
+	while(1)
+	{
+	if(!(PINB&0x02))
+	{
+		while (!(PINB&0x02)); //it has two value,either 1 or 0.
+		C++;
+		if(C==1)
+		{
+			PORTB|=0x01;	 //TURNIING LED ON
+		}
+
+		if(C==2)
+		{
+			PORTB&=0xfe; //turning off the last bit(output bit), the other input bits remain unchanged
+			C=0;
+		}
+	}
+	}
+
+}
+
