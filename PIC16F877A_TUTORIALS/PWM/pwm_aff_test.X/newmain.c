3d430044a6de4e1cc16784825571a9fef83cc396
Ajay Mathews <37361585+ajaymathews@users.noreply.github.com>
2023-03-25 17:19:32 +0100

    Add files via upload

---
+++ PIC16F877A_TUTORIALS/PWM/pwm_aff_test.X/newmain.c
@@ -0,0 +1,52 @@
+/*
+ * File:   newmain.c
+ * Author: AJAY
+ *
+ * Created on 12 November, 2018, 12:04 PM
+ */
+/*Actually the tmr2 timer started(we could initialize it to to zero if needed), then the timer will be incremented till it
+ reaches the pr2 value and after that it again starts from zero, thus from zero to pr2 is the time period of the wave.with 100% dutycycle
+ to generate a pwm from thus signal we write values to ccpr1l and ccp1con<4,5> bits , the lsb of the pwm 10 bit data is stored in 
+ * the ccp1con<4,5> bits. thus the pwm is acheived. */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 1000000
+//REG=[ , , , , , , ,]     VAL=1011100110
+//REG=VAL     -> 1,0 [1,1,1,0,0,1,1,0] 
+//REG=VAL>>2  -> [1,0,1,1,1,0,0,1] 1,0
+
+
+void dutycycle(unsigned int dc)
+	{
+		CCPR1L=(dc>>2);   /* the MSB is stored into the CCPR1L register by shifting the last last outside
+                           since PWM REG has 10 bit and CCPRL REG stores only 8 bit, so LSB must be kicked out
+                           this value doesnt go anywhere just not included in this register*/
+        
+        CCP1CON&=0xCF;    /*the CCP1CON<5,4>(5th and 4th bit) reg is the place where we store the MSB value 
+                          that have been kicked out from the CCPR1L reg,for that we place zero in the 
+                          5th and 4th position to recieve this value and other bits unchanged by AND ing with 1s
+                          all the other bits than 4&5 */
+        
+		CCP1CON|=(0x30&(dc<<4));  /* to get the last LSB value, we shift the bits rightward by 4 bits, thus the bits
+                                   already on outside(because cant store 10 bits in 8-bit register) of the register 
+                                   will be moved to the front position 4 times and thus that 2 bits will reach the 5th and 4th position */  
+	}
+
+void main()
+{
+    int i;
+
+	TRISC2=0;
+    TRISB0=0;
+	CCP1CON=0x0C;     // setting the values to reg, that is the 2nd and 3rd bit set as 1 to be in PWM mode  
+	PR2=0xFF;         /* setting the pr2 asc high  value;
+                        when tmr2 reaches and equals pr2, then tmr2 resets ,
+                       dutycycle is latched from ccprl to ccprh(read only register)*/
+	T2CON=0x04;      // setting the timer2  
+	dutycycle(0);    //initializing dutycycle
+	T2CON|=0x04;
+
+        dutycycle(100);
+        while(1);
+}
---
+++ PIC16F877A_TUTORIALS/TASK/LCD_4BIT.X/newmain.c
@@ -0,0 +1,157 @@
+/*
+ * 
+ * File:   newmain.c
+ * Author: AJAY
+ *
+ * Created on 1 January, 2020, 10:07 AM
+ */
+
+
+#include <xc.h>
+
+#define _XTAL_FREQ 16000000L
+#define RS RB0
+#define EN RB2
+void send_str(char *p);
+char timer_value[]="00:00:00";
+char ms='0',s='0',m='0';
+
+void cmd_4bit(int x)
+{
+    PORTB& = 0x0F;
+    RS=0;
+    EN=0;
+    PORTB |= (x & 0xF0);
+    EN=1;
+    __delay_ms(10);
+    EN=0;
+    
+
+    
+    PORTB& = 0x0F;
+    PORTB |= ((x<<4) & 0xF0);
+    EN=1;
+    __delay_ms(10);
+    EN=0;
+    
+    
+}
+void lcd_init()
+{
+	//cmd_2bit(0x33);
+	//cmd_2bit(0x32);
+	cmd_4bit(0x02);
+	cmd_4bit(0x01);
+	//cmd_2bit(0x06);
+	cmd_4bit(0x0c);
+	cmd_4bit(0x80);
+}
+
+
+void data_4bit(char y)
+{
+    
+	PORTB& = 0x0F;//clear already existing data
+    RS=1;
+    EN=0;
+    PORTB |= (y & 0xF0);
+    EN=1;
+    __delay_ms(10);
+    EN=0;
+    
+
+    
+    PORTB& = 0x0F;
+    PORTB |= ((y<<4) & 0xF0);
+    EN=1;
+    __delay_ms(10);
+    EN=0;
+    
+}
+void send_str(char *p)
+{
+	while(*p!='\0')
+	{
+		data_4bit(*p);
+		p++;
+	}
+}
+void uart_init()
+{
+	TXSTA=0x24;//0b0010,0100
+	RCSTA=0x90;
+	SPBRG=103;
+}
+
+char receive()
+{
+	while(!RCIF);
+	char b=RCREG;
+	return b;
+}
+
+void transmit(char b)
+{	
+	TXREG=b;
+	while(!TRMT);
+}
+
+void str_transmit(char *p)
+{
+	while(*p!='\0')
+		{
+			transmit(*p);
+			p++;
+	 	}    
+}
+
+void str_receive()
+{
+	char c[15];
+	int i;
+	for(i=0;i<15;i++)
+	{
+	c[i]=receive();
+	if(c[i]==' ')
+	{
+		c[i]='\0';
+		break;
+    }
+}
+}
+
+void main()
+{
+	TRISB=0x00;
+    T1CON = 0b00110001;
+    TRISD1=0; 
+    int count=0;
+	lcd_init();
+    while(1)
+    {
+     if(count==7)
+     {
+         ms=ms+1;
+         if(ms==100)
+         {
+             s++;
+             if(s==60)
+             {
+                 m++;
+             }
+         }
+         sprintf(timer_value,"%d:%d:%d",m,s,ms);
+                                              
+     }
+    if(TMR1IF)
+    {
+     TMR1IF = 0;
+     count ++;
+     }
+ 
+    }
+   
+    send_str(timer_value);
+    while(1);
+         
+}
