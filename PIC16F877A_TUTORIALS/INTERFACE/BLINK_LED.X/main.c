f900313632505b73de1b72ffa28bcd47ae42693c
Ajay Mathews <37361585+ajaymathews@users.noreply.github.com>
2023-03-25 17:14:38 +0100

    Add files via upload

---
+++ PIC16F877A_TUTORIALS/INTERFACE/BLINK_LED.X/main.c
@@ -0,0 +1,40 @@
+/*
+ * File:   newmain.c
+ * Author: AJAY
+ *
+ * Created on 9 October, 2018, 2:40 PM
+ */
+
+#include<pic.h>   // include pic headers
+#include <xc.h>   // include xc8 complier header
+#define _XTAL_FREQ 16000000 // to include the delay fuction
+__CONFIG(0X3F7A);    //  Equalent to setting  CONFIGURATION bits
+
+void main(void) 
+{
+    TRISB=0X00;   // SETTING THE B PORT AS OUTPUT
+    TRISC4=0;  // SSETTING 4th PIN as the OUTPUT
+    
+//    //TURNING THE COMPLETE B PORT ON/OFF
+//    while(1)
+//    {
+//    PORTB=0XFF;    // TURNING LIGHTS ON ALL PORTS
+//    __delay_ms(100); // delay in microseconds
+//    
+//    // __delay_us(xxx);  // delay in nano seconds
+//    
+//    PORTB=0X00;   // TURNING LIGHTS OFF ALL PORTS
+//   __delay_ms(100);
+//    }
+    
+     //TURNING 4th PIN IN THE C PORT ON/OFF
+//    while(1)
+//    {
+//        RC4=0;
+//        __delay_ms(100);
+//        RC4=1;
+//        __delay_ms(100);   
+//    }
+    
+    return;
+}
---
+++ PIC16F877A_TUTORIALS/INTERFACE/SWITCH.X/main.c
@@ -0,0 +1,30 @@
+/*
+ * File:   main.c
+ * Author: AJAY
+ *
+ * Created on 20 October, 2018, 12:19 PM
+ */
+
+#include<pic.h>   // include pic headers
+#include <xc.h>   // include xc8 complier header
+#define _XTAL_FREQ 16000000 // to include the delay fuction
+__CONFIG(0X3F7A);    //  Equalent to setting  CONFIGURATION bits
+
+void main(void) {
+    TRISB1=0;  // setting the b1 pin in the b port as output to connect led
+    TRISB2=1;  // setting the b2 pin in the b port as input to read switch
+    while(1)
+    {
+    if(RB2==0)  // checking the switch is on, by recieving 0. since the switch
+                //is connected in active low
+    {
+        RB1=1;   // turning led ON
+    }
+    else
+    {
+        RB1=0;  // turning led OFF
+    }
+    
+    }
+    return;
+}
